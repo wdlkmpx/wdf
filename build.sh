664cfed080d83e8d9563a271983463b1953add08
wdlkmpx <wdlkmpx@gmail.com>
2023-01-12 22:32:05 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,11 +18,11 @@ set -ex
 docker buildx create --name wbuilder
 docker buildx use wbuilder
 docker buildx inspect --bootstrap
-docker buildx build --push --platform linux/s390x --tag wdlkmpx/gcc:s390x_alpine . s390x_alpine
+docker buildx build --push --platform linux/s390x --tag wdlkmpx/gcc:s390x_alpine s390x_alpine
 docker buildx rm wbuilder
  
 #docker buildx create --name wbuilder
 #docker buildx use wbuilder
 #docker buildx inspect --bootstrap
-#docker buildx build --push --platform linux/s390x --tag wdlkmpx/gcc:s390x_debian . s390x_debian
+#docker buildx build --push --platform linux/s390x --tag wdlkmpx/gcc:s390x_debian s390x_debian
 #docker buildx rm wbuilder
